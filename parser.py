3593631f593f9aeadd458bcda8d3e85477a7276d
Sarper <sarper90@gmail.com>
2014-02-25 17:50:07 -0500

    ..

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 __author__ = 'Sarper'
 
 from multinomial import *
+import pydecode.hyper as ph
 
 
 class Counts:
@@ -44,15 +45,54 @@ class Counts:
                     self.binary_rule_with_nonterm[terms[2]][
                         (terms[3], terms[4])] = count
 
+
 class Parser:
 
     def __init__(self, table_of_multinomials, counts):
         self.table_of_multinomials = table_of_multinomials
         self.counts = counts
 
-    def parse(self,input_file):
+    def parse(self, sentence):
+        words = sentence.strip().split(" ")
+        n = len(words)
+        nodes = {}
+        for i, word in enumerate(words):
+            if word not in self.counts.term or self.counts.term[word] < 5:
+                words[i] = '_RARE_'
+        sentence_graph = ph.Hypergraph()
+        with sentence_graph.builder() as b:
+            for i, word in enumerate(words, start=1):
+                relevant_rules = (rule for rule in self.counts.unary.iterkeys()
+                                  if rule[1] == word)
+                nodes[(word, i, i)] = b.add_node(label=word)
+                for rule in relevant_rules:
+                    node_str = "%s:%d:%d" % (rule[0], i, i)
+                    edge_str = "%s:%s" % (rule[0], rule[1])
+                    print node_str, edge_str
+                    nodes[(rule[0], i, i)] = b.add_node([([nodes[(rule[1], i, i)]], edge_str)],
+                                                        label=node_str)
+
+            for l in xrange(1, n):
+                for i in xrange(1, n-l+1):
+                    j = i+l
+                    for nonterminal in self.counts.binary_rule_with_nonterm.iterkeys():
+                        edgelist = []
+                        for rule in self.counts.binary_rule_with_nonterm[nonterminal].iterkeys():
+                            for s in xrange(i, j):
+                                if (rule[0], i, s) in nodes.keys() and (rule[1], s+1, j) in nodes.keys():
+                                    edgelist.append(([nodes[(rule[0], i, s)], nodes[(rule[1], s+1, j)]], nonterminal+':'+rule[0]+':'+rule[1]))
+                        if edgelist:
+                            nodes[(nonterminal, str(i), str(j))] = b.add_node(edgelist, label=nonterminal+':'+str(i)+':'+str(j))
+            print nodes.keys()
+
+    def parse_file(self, input_file):
+        with open(input_file) as f:
+            for sentence in f:
+                self.parse(sentence)
+
 
 def main():
+
     counts = Counts()
     counts.read_counts('replaced_cfg.counts')
     table_of_multinomials = TableOfMultinomial()
@@ -61,7 +101,8 @@ def main():
             counts.binary_rule_with_nonterm.get(nonterm, {}).copy()
         nonterm_counts.update(counts.unary_rule_with_nonterm.get(nonterm, {}))
         table_of_multinomials.create(nonterm, nonterm_counts)
-
+    parser = Parser(table_of_multinomials, counts)
+    parser.parse_file('sentence.dat')
 
 if __name__ == "__main__":
     main()
